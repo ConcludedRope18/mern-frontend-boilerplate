1f79141bcb9f02cf0ac1ac5d80ecf375428355a9
voicu-bogdan <bogdan.iulian.voicu@gmail.com>
2025-05-07 17:14:55 +0300

    made framework for objects to connect to a faux-database for easier implementation later

--- src/components/transaction_card.jsx
+++ src/components/transaction_card.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import "./transaction_card.css"
 
 const Card = ({name, amount, imageUrl}) => {
+  const transactionType = amount >= 0 ? "Received" : "Sent";
+  amount = Math.abs(amount)
     return (
       <div className="card">
         <img className="pfp" src={imageUrl}></img>
-        <div className="name">{name}</div>
-        <div className="message">{amount}</div>
+        <div className="name">{name ?? "Client"}</div>
+        <div className="message">{transactionType} ron {amount}</div>
       </div>
     );
   };
